fcd279771c3c429ab1aac9f787d059db2c0d624e
fit-s-u-m <nehemiahmaths@gmail.com>
2024-07-09 17:41:11 +0300

    refactor the code

--- main.js
+++ main.js
@@ -1,73 +1,5 @@
 import * as PIXI from 'pixi.js'
 
-class Tetris {
-  constructor() {
-    const I = [[0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0]]
-    const O = [[1, 1, 0], [1, 1, 0]]
-    const T = [[0, 1, 0], [1, 1, 1]]
-    const S = [[0, 1, 1], [1, 1, 0]]
-    const Z = [[1, 1, 0], [0, 1, 1]]
-    const J = [[1, 0, 0], [1, 1, 1]]
-    const L = [[0, 0, 1], [1, 1, 1]]
-    const tetrisGrid = {
-      0: { val: I, color: "#ff0000" },
-      1: { val: O, color: "#00ff00" },
-      2: { val: T, color: "#0000ff" },
-      3: { val: S, color: "#00ffff" },
-      4: { val: Z, color: "#ffff00" },
-      5: { val: J, color: "#ff00ff" },
-      6: { val: L, color: "#000000" }
-    }
-    this.tetris = {
-      now: null,
-      next: null
-    }
-
-    let rand1 = Math.floor(Math.random() * 7)
-    let rand2 = Math.floor(Math.random() * 7)
-    // choose random tetris
-    this.tetris.now = tetrisGrid[rand1]
-    this.tetris.next = tetrisGrid[rand2]
-    this.tetrisDrawing = new PIXI.Container(); // create tetris container
-  }
-  showTetris(app, { x, y }, grid) {
-    const cellSize = grid[1][0].x - grid[0][0].x
-    const tetris_now = this.tetris.now
-    const numRow = tetris_now.val.length
-    // get pos from coordinate for starting point
-    const gridX = grid[x][y].x
-    const gridY = grid[x][y].y
-
-    for (let i = 0; i < numRow; i++) { // for every row
-
-      const numCol = tetris_now.val[i].length
-      const tetrisYPos = gridY + i * cellSize
-
-      for (let j = 0; j < numCol; j++) { // for every  column
-
-        const tetrisXPos = gridX + j * cellSize
-
-        if (tetris_now.val[i][j] == 1) { // if that tetris have value 1 
-          const rect = new PIXI.Graphics()
-            .rect(tetrisXPos, tetrisYPos, cellSize, cellSize)
-            .fill(tetris_now.color);
-          this.tetrisDrawing.addChild(rect)
-        }
-      }
-    }
-    app.stage.addChild(this.tetrisDrawing);
-  }
-  rotate(angle) {
-    this.tetrisDrawing.rotation = angle
-  }
-  moveDown(dt, speed) {
-    this.tetrisDrawing.position.y += dt * speed
-  }
-  collidedWith(tetris) { //TODO
-    return  // return if collided 
-  }
-}
-
 class Grid {
   constructor({ x, y }, { numRow, numCol }, maxHeight) {
     this.grid = []
@@ -82,7 +14,7 @@ class Grid {
     this.size = size
   }
 
-  createGrid() {
+  create() {
     for (let i = 0; i < this.numCol; i++) {
       this.grid[i] = []
       for (let j = 0; j < this.numRow; j++) {
@@ -94,26 +26,282 @@ class Grid {
     return this.grid
   }
 
-  showGrid(app) {
-    const border = new PIXI.Graphics() // grid border
-      .rect(this.position.x, this.position.y, this.width, this.height)
-      .setStrokeStyle(5, 0xff0000, 1, 0)
-      .stroke(0xff0000, 5)
-    border.zIndex = 1  // show border on top
-    app.stage.addChild(border)
+  show() {
+    const container = new PIXI.Container()
     for (let i = 0; i < this.grid.length; i++) {
       for (let j = 0; j < this.grid[i].length; j++) {
         const rect = new PIXI.Graphics()
           .rect(this.grid[i][j].x, this.grid[i][j].y, this.size, this.size)
           .stroke(0x000000)
-        app.stage.addChild(rect)
+        container.addChild(rect)
       }
     }
+    return container
+  }
+}
+
+class TetrominoFactory {
+  static createShape(type) {
+    type = type.toUpperCase()
+    switch (type) {
+      case "I":
+        return {
+          shape: [[1, 1, 1, 1]],
+          color: "#FD3F59" // red
+        }
+      case 'O':
+        return {
+          shape: [[1, 1], [1, 1]],
+          color: "#800080"  // purple
+        }
+      case 'T':
+        return {
+          shape: [[0, 1, 0], [1, 1, 1]],
+          color: "#ffff00" // yellow
+        }
+      case 'S':
+        return {
+          shape: [[0, 1, 1], [1, 1, 0]],
+          color: "#ff7f00" // orange
+        }
+      case 'Z':
+        return {
+          shape: [[1, 1, 0], [0, 1, 1]],
+          color: "#00ffff" // cyan
+        }
+      case 'J':
+        return {
+          shape: [[1, 0, 0], [1, 1, 1]],
+          color: "#7f7f7f" // gray
+        }
+      case 'L':
+        return {
+          shape: [[0, 0, 1], [1, 1, 1]],
+          color: "#00ff00"
+        }
+      default:
+        throw new Error('Unknown Tetromino type');
+    }
+  }
+}
+
+class Tetromino {
+  constructor(shape, color, grid) {
+    this.shape = shape;
+    this.color = color;
+    this.position = { x: 0, y: 0 };
+    this.rotation = 0;
+    this.speed = 2;
+    this.cellSize = null;
+    this.container = new PIXI.Container();
+    this.grid = grid
+  }
+  show() {
+    const cellSize = this.grid[1][0].x - this.grid[0][0].x
+    this.cellSize = cellSize
+    const numRow = this.shape.length
+    // get pos from coordinate for starting point
+    const gridX = this.grid[this.position.x][this.position.y].x
+    const gridY = this.grid[this.position.x][this.position.y].y
+
+    for (let i = 0; i < numRow; i++) { // for every row
+
+      const numCol = this.shape[i].length
+      const tetrominoYPos = gridY + i * cellSize
+
+      for (let j = 0; j < numCol; j++) { // for every  column
+
+        const tetrominoXPos = gridX + j * cellSize
+
+        if (this.shape[i][j] == 1) {
+          const rect = new PIXI.Graphics()
+            .roundRect(tetrominoXPos, tetrominoYPos, cellSize, cellSize, 5)
+            .fill(this.color)
+            .stroke("#000");
+
+          this.container.addChild(rect)
+        }
+      }
+    }
+    return this.container
+  }
+  updateContainer(shape = null, color = null) {
+    if (shape && color) {
+      this.shape = shape
+      this.color = color
+    }
+    this.container.removeChildren()
+    this.show()
+  }
+
+
+  rotate() {
+    const newShape = this.shape[0].map((_, index) =>
+      this.shape.map(row => row[index])
+    ).reverse();
+    this.updateContainer(newShape, this.color)
+  }
+  setPosition(x, y) {
+    this.container.position = { x: x * this.cellSize, y: y * this.cellSize }
+  }
+
+  moveDown() {
+    this.container.position.y += this.speed
+  }
+  moveLeft() {
+    if (this.container.position.x >= this.cellSize)
+      this.container.position.x -= this.cellSize;
+  }
+
+  moveRight() {
+    if (this.container.position.x < (this.grid.length - this.shape[0].length) * this.cellSize)
+      this.container.position.x += this.cellSize;
+  }
+}
+
+// Game logic
+class Game {
+  constructor(mainGrid, sideGrid, app) {
+    this.mainGrid = mainGrid;
+    this.sideGrid = sideGrid;
+
+    // array of the grid
+    this.nowGrid = mainGrid.create();
+    this.nextGrid = sideGrid.create();
+    this.app = app;
+
+    // create the shape
+    const currentShape = TetrominoFactory.createShape(this.getRandomTetrominoType());
+    const nextShape = TetrominoFactory.createShape(this.getRandomTetrominoType());
+
+    // create the tetromino
+    this.currentTetromino = new Tetromino(currentShape.shape, currentShape.color, this.nowGrid);
+    this.nextTetromino = new Tetromino(nextShape.shape, nextShape.color, this.nextGrid);
+
+    // not moving  tetromino after they fall
+    this.staticTetrominoShape = []
+    this.staticTetrominoContainer = new PIXI.Container()
+
+    // stage everything
+    this.app.stage.addChild(
+      this.sideGrid.show(),
+      this.mainGrid.show(),
+      this.currentTetromino.show(),
+      this.nextTetromino.show(),
+      this.staticTetrominoContainer
+    )
+  }
+  getRandomTetrominoType() {
+    const types = ['I', 'O', 'T', 'S', 'Z', 'J', 'L'];
+    return types[Math.floor(Math.random() * (types.length - 1))];
+  }
+  spawnNewTetromino() {
+    this.currentTetromino.updateContainer(this.nextTetromino.shape, this.nextTetromino.color)
+    this.currentTetromino.setPosition(0, 0)
+    const nextShape = TetrominoFactory.createShape(this.getRandomTetrominoType());
+    this.nextTetromino.updateContainer(nextShape.shape, nextShape.color)
+  }
+  tetrominoLanded() {
+    const topRightCorner = this.nowGrid[0][0]
+    const cellSize = this.nowGrid[1][0].x - this.nowGrid[0][0].x
+    const gridHeight = cellSize * this.nowGrid[0].length
+    const currentY = this.currentTetromino.container.getBounds().maxY
+    const ground = topRightCorner.y + gridHeight
+
+    if (currentY > ground || this.collidedWithStatic()) { // bottom
+      return true
+    }
+    return false
+  }
+  drawStaticShapes(shape, pos, col, container) {
+    const cellSize = this.nowGrid[1][0].x - this.nowGrid[0][0].x
+    const numRow = shape.length
+
+    const gridX = pos.minX
+    const gridY = pos.minY
+
+    for (let i = 0; i < numRow; i++) { // for every row
+
+      const numCol = shape[i].length
+      const tetrominoYPos = gridY + i * cellSize
+
+      for (let j = 0; j < numCol; j++) { // for every  column
+
+        const tetrominoXPos = gridX + j * cellSize
+
+        if (shape[i][j] == 1) {
+          const rect = new PIXI.Graphics()
+            .roundRect(tetrominoXPos, tetrominoYPos, cellSize, cellSize, 5)
+            .fill(col)
+            .stroke("#000");
+
+          container.addChild(rect)
+        }
+      }
+    }
+  }
+  reDrawStatic() {
+    this.staticTetrominoContainer.removeChildren()
+    this.staticTetrominoShape.forEach(obj => {
+      this.drawStaticShapes(obj.shape, obj.pos, obj.col, this.staticTetrominoContainer)
+    })
+  }
+  collidedWithStatic() { //TODO
+    for (let movingRect of this.currentTetromino.container.children) {
+      for (let staticRect of this.staticTetrominoContainer.children) {
+        const selfBound = movingRect.getBounds()
+        const otherBound = staticRect.getBounds()
+        const xOverlap = selfBound.maxX >= otherBound.minX && selfBound.minX <= otherBound.maxX
+        const yOverlap = selfBound.maxY >= otherBound.minY && selfBound.minY <= otherBound.maxY
+        if (yOverlap && xOverlap) {
+          console.log("conlided")
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  // Update the game state
+  update() {
+    this.currentTetromino.moveDown()
+    if (this.tetrominoLanded()) {
+      this.staticTetrominoShape.push({
+        shape: this.currentTetromino.shape,
+        pos: this.currentTetromino.container.getBounds(),
+        col: this.currentTetromino.color
+      })
+      this.spawnNewTetromino()
+    }
+    this.reDrawStatic()
   }
 }
 
 
 
+class InputHandler {
+  constructor(game) {
+    this.game = game
+    window.addEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  handleKeyDown(event) {
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.game.currentTetromino.moveLeft();
+        break;
+      case 'ArrowRight':
+        this.game.currentTetromino.moveRight();
+        break;
+      case 'ArrowDown':
+        this.game.currentTetromino.moveDown();
+        break;
+      case 'ArrowUp':
+        this.game.currentTetromino.rotate();
+        break;
+    }
+  }
+}
 const main = async () => {
   const app = new PIXI.Application()
   await app.init({
@@ -127,32 +315,15 @@ const main = async () => {
   )
   const smallGrid = new Grid(
     { x: app.screen.width / 2 + 400, y: app.screen.height / 2 },
-    { numRow: 4, numCol: 3 },
+    { numRow: 4, numCol: 4 },
     150
   )
-  const nowGrid = mainGrid.createGrid()
-  const nextGrid = smallGrid.createGrid()
-
-  const now = new Tetris()
-  const next = new Tetris()
-
-  mainGrid.showGrid(app)
-  smallGrid.showGrid(app)
-
-
-  now.showTetris(
-    app,
-    { x: 1, y: 0 }, // position in the grid
-    nowGrid
-  )
-  next.showTetris(
-    app,
-    { x: 0, y: 0 },
-    nextGrid
-  )
+  const game = new Game(mainGrid, smallGrid, app)
+  const inputHandler = new InputHandler(game)
   app.ticker.add(time => {
-    now.moveDown(time.deltaTime, 0.1)
+    game.update(time.deltaTime)
   })
+
   document.body.appendChild(app.canvas);
 }
 main()
