0239d8f2cbf67fad76bb21594ae13b3615e6d1dd
fit-s-u-m <nehemiahmaths@gmail.com>
2024-07-16 01:12:23 +0300

    add types for score and text

--- componets/types.ts
+++ componets/types.ts
@@ -2,6 +2,7 @@ import { Renderer } from "./renderer";
 import { Block } from "./block";
 import { Grid } from "./grid";
 import { EventListener } from "./eventListener";
+import { Score } from "./score";
 import *  as PIXI from "pixi.js"
 
 export type EVENT = "resize" | "keyboard"
@@ -9,9 +10,12 @@ export type PIXICONTAINER = PIXI.Container
 export type RENDERER = Renderer
 export type BLOCK = Block
 export type GRID = Grid
+export type SCORE = Score
 export type LISTENER = { obj: any, event: EVENT }[]
-export type CALLBACK = PIXI.TickerCallback<any>
+export type CALLBACK = FrameRequestCallback | PIXI.TickerCallback<any>
 export type EVENTLISTENER = EventListener
 export type SIDE = "right" | "left" | "both"
 export type TIME = "now" | "future"
 export type BINARY = 0 | 1
+export type TEXT = PIXI.Text
+export type TEXTSTYLE = PIXI.TextStyle
